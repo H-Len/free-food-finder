c396eae6ce49036869dc832bfaedb7a1fe3331eb
brooke.kullberg <brookekullberg@gmail.com>
2019-04-15 15:06:15 -0700

    add object constructor for FoodLocation

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Business Logic for FOOD-location OBJECTS
+
+function FoodLocation() {
+  name: this.name,
+  address: this.address,
+  hours: this.hours,
+  date: this.date,
+  website: this.website,
+  type: this.type,
+  restrictions: this.restrictions,
+  other: this.other
+}
+
+
+//UI for Daily calendar on index.html
