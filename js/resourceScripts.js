1497691627780a1cb464f6b98c7f020c949f234d
Student <example@example.com>
2019-04-16 15:03:48 -0700

    add Resources page; add circles and more in CSS to go with Resources

---
+++ js/resourceScripts.js
@@ -0,0 +1,19 @@
+//Constructor for adding resources and info
+
+function Resource(name, phoneNumber, link) {
+  this.name = name;
+  this.phoneNumber = phoneNumber;
+  this.siteLink = siteLink;
+}
+
+// Resource.prototype.numberDashes = function() {
+//  //add dashes
+//   if (phoneNumber[3] != '-') {
+//     phoneNumber = phoneNumber[3].prepend("-")
+//   }
+//   if (phoneNumber[7] != '-') {
+//     phoneNumber phoneNumber[7].prepend("-");
+//   }
+// }
+
+var trimetResource = new Resource(tripPlannerTrimet, 5032387433, https://trimet.org/ride/m/planner_form.html )
