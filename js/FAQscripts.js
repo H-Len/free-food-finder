53cfb6af1d24a9e6932d86ea22c98926082091e2
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-16 22:53:21 -0700

    clean up collapsible set code

--- js/FAQscripts.js
+++ js/FAQscripts.js
@@ -4,8 +4,11 @@ $(document).ready(function(){
     for (var i = 0; i < question.length; i++) {
     question[i].addEventListener("click", function() {
       this.classList.toggle("active");
+      // nextElementSibling property returns the element immediately following the specified element, in the same tree level.
       var answer = this.nextElementSibling;
+      // The maxHeight property sets or returns the maximum height of an element. It will only work on block level elements or elements of fixed/absolute value
       if (answer.style.maxHeight){
+      // scrollHeight property returns the entire height of an element in pixels, including padding, but not the border, scrollbar or margin.
         answer.style.maxHeight = null;
       } else {
         answer.style.maxHeight = answer.scrollHeight + "px";
