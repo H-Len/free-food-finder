ac005f7f99636ad0a84af629c14b4202ea9c1b8e
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-16 21:38:23 -0700

    edit collapsible set

---
+++ js/FAQscripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  var question = document.getElementsByClassName("collapsible");
+  var i;
+    for (i = 0; i < question.length; i++) {
+    question[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var answer = this.nextElementSibling;
+      if (answer.style.maxHeight){
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      }
+    });
+  }
+});
